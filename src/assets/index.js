cf518f0796a670a56ad5a1cdd26aa37dbd90191d
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2023-11-17 17:09:09 +0200

    Level 2: added react and react dom

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export const dataProtection = require('./images/data_protection.png');
+export const fraudPrevention = require('./images/fraud_prevention.png');
+export const itSecurity = require('./images/it_security.png');
+export const wphg = require('./images/wphg.png');
